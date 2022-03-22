26913a19bfb1e44f786d9a1b551e1cce9b3c97ed
Shriram Ravi <shriramravi96@gmail.com>
2022-03-21 20:26:32 -0400

    Modified logic

--- A2/a2.hpp
+++ A2/a2.hpp
@@ -14,32 +14,18 @@
 
 struct suffix_node
 {
-    long long int start_index = 0;
-    long long int node_length = 0;
+    std::string node_string = "";
+
     short int next_list_length = 0;
+
+    long long int node_string_length = 0;
 
     std::vector<suffix_node *> next_list = {NULL, NULL, NULL, NULL, NULL, NULL};
 
-    suffix_node(const long long int start = 0, 
-                const long long int length = 0, 
-                const short int node_list_length = 0, 
-                const std::vector<suffix_node *> node_list = {NULL, NULL, NULL, NULL, NULL, NULL})
+    suffix_node(const std::string string = "")
     {
-        start_index = start;
-        node_length = length;
-        next_list_length = node_list_length;
-
-        for(int i = 0; i < node_list.size(); i++)
-        {
-            next_list.push_back(node_list.at(i));
-        }
-    }
-
-    void refresh_nodes()
-    {
-        std::vector<suffix_node *> refreshed_next_list(6, NULL);
-        next_list = refreshed_next_list;
-        next_list_length = 0;
+        node_string = string;
+        node_string_length = node_string.length();
     }
 
 };
@@ -64,119 +50,74 @@ void create_suffix_tree(std::string &text)
 {
     
     long long int text_len = text.find("$") + 1;
-    if(text_len == 0)
-    {
-        text = text + "$";
-        text_len = text.length();
-    }
+    text = text.substr(0, text_len);
 
-    std::cout << "Text Length: " << text_len << "\tTotal Length:" << text.length() << std::endl;
+    // std::cout << "Text Length: " << text_len << std::endl;
 
-    suffix_node* root = new suffix_node();
+    suffix_node* root = new suffix_node("");
+    root->next_list[HASH(text[0])] = new suffix_node(text);
+    root->next_list_length += 1;
 
-    long long int internal_nodes = 0;
-    long long int outgoing_edges = 0;
+    long long int internal_nodes = 1;
+    long long int outgoing_edges = 1;
 
-    for(int i = 0; i < text_len; i++)
+    for(int i = 1; i < text_len; i++)
     {
         // std::cout << std::endl << "Started suffix: " << i << std::endl;
         suffix_node* current_node = root;
-        int previous_length = root->node_length;
 
-        std::cout << std::endl << "STARTED Adding: " << text.substr(i, text_len - i) << " to the Trie" << std::endl;
-        for(int j = i; j < text_len; j++)
+        // std::cout << std::endl << "STARTED Adding: " << text.substr(i, text_len - i) << " to the Trie" << std::endl;
+        long long int j = i;
+        while(j < text_len)
         {
-            long long int node_length = current_node->node_length;
-
-            std::cout   << "\tj : " << j << "\t"
-                        << "i: "  << i << "\t"
-                        << "s: "  << current_node->start_index << "\t"
-                        << "length: "  << current_node->node_length << "\t"
-                        << "String: " << text.substr(current_node->start_index, node_length) << " vs " << text.substr(j, text_len - j) << std::endl;
-
-            std::cout << "Previous Length: " << previous_length << std::endl;
-            int collateral = 0;
-            if(previous_length == 1)
+            if(current_node->next_list[HASH(text[j])] != NULL)
             {
-                collateral = 1;
-            }
-            previous_length = current_node->node_length;
+                suffix_node* child_node = current_node->next_list[HASH(text[j])];
+                std::string node_string = child_node->node_string;
+                long long int node_string_length = child_node->node_string_length;
 
-            if(((j - i) < node_length + collateral) && (node_length > 1))
-            {
-                if(text[j] == text[j - i + current_node->start_index + collateral])
+                long long int k = j + 1;
+
+                while(k - j < node_string_length && text[k] == node_string[k-j])
                 {
-                    std::cout << "\t\tEqualling at j: " << j << "\tWith letter: " << text[j] << std::endl;
-                    continue;
+                    k += 1;
+                }
+
+                if(k - j == node_string_length)
+                {
+                    current_node = child_node;
+                    j = k;
                 }
                 else
                 {
-                    long long int split_start  = j - i + current_node->start_index + collateral;
-                    long long int split_length = current_node->node_length - j + i + collateral;
+                    suffix_node* split_node = new suffix_node(node_string.substr(0, k - j));
+                    split_node->next_list[HASH(text[k])] = new suffix_node(text.substr(k, text_len - k));
+                    split_node->next_list[HASH(node_string[k - j])] = child_node;
+                    split_node->next_list_length += 2;
 
-                    std::cout << "\t\tNOT equalling at j: " << j << "\tWith letter: " << text[j] << " != " << text[split_start] << std::endl;
-
-                    std::cout << "\t\tNew split: " << text.substr(current_node->start_index, node_length - split_length) << " | " << text.substr(split_start, split_length) 
-                    << "\ti: " << i << "\tj: " << j << "\tcurrent_start: " << current_node->start_index << std::endl;
-
-                    suffix_node* split_node = new suffix_node(split_start, split_length, current_node->next_list_length, current_node->next_list);
-
-                    current_node->refresh_nodes();
-
-                    std::cout << "\t\tAdding split node: " << text.substr(split_start, split_length) << "\tStarting: " << text[split_start] << "\tStart: " << split_start << "\tLength: " << split_length << std::endl;
-                    current_node->next_list[HASH(text[split_start])] = split_node;
-                    current_node->next_list_length += 1;
+                    child_node->node_string = node_string.substr(k - j);
+                    child_node->node_string_length = child_node->node_string.length();
+                    current_node->next_list[HASH(text[j])] = split_node;
+                    split_node->next_list_length += 1;
 
                     internal_nodes += 1;
-                    std::cout << "\t\t\tAdding Internal Node. Internal Nodes = " << internal_nodes << std::endl;
-
-                    outgoing_edges += 1;
-                    std::cout << "\t\t\tAdding Outgoing Edge. Outgoing Edges = " << outgoing_edges << std::endl;
-
-                    current_node->node_length -= split_length;
-
-                    std::cout << "->" << text.substr(split_start, split_length) << std::endl;
+                    outgoing_edges += 2;
                 }
             }
             else
             {
-                std::cout << "\t\tCan't compare because of range. Current Node: " << text.substr(current_node->start_index, node_length) << std::endl;
-            }
-
-            if(current_node->next_list[HASH(text[j])] == NULL)
-            {
-                std::cout << "\t\tCreating new node at j: " << j << "\tWith letter: " << text[j] << "\tSubstring: " << text.substr(j, text_len - j) << std::endl;
-                std::cout << "->" << text.substr(j, text_len - j) << std::endl;
-
                 if(current_node->next_list_length == 0)
                 {
                     internal_nodes += 1;
-                    std::cout << "\t\t\tAdding Internal Node. Internal Nodes = " << internal_nodes << std::endl;
                 }
-
-                suffix_node* new_node = new suffix_node(j, text_len - j);
-                current_node->next_list[HASH(text[j])] = new_node;
-
+                current_node->next_list[HASH(text[j])] = new suffix_node(text.substr(j));
                 current_node->next_list_length += 1;
-
                 outgoing_edges += 1;
-                std::cout << "\t\t\tAdding Outgoing Edge. Outgoing Edges = " << outgoing_edges << std::endl;
-
-                current_node = current_node->next_list[HASH(text[j])];
-
-                break;
             }
-            else
-            {
-                std::cout << "\t\tTraveling to next node from j: " << j << "\twith letter: " << text[j] << "\tFrom Node: " << text.substr(current_node->start_index, current_node->node_length) << std::endl;
-                current_node = current_node->next_list[HASH(text[j])];
-            }
-
+            // std::cout << "FINISHED Adding: " << text.substr(i, text_len - i) << " to the Trie" << std::endl;
+            // std::cout << "Internal Nodes: " << internal_nodes << std::endl;
+            // std::cout << "Outgoing Edges: " << outgoing_edges << std::endl;
         }
-        std::cout << "FINISHED Adding: " << text.substr(i, text_len - i) << " to the Trie" << std::endl;
-        std::cout << "Internal Nodes: " << internal_nodes << std::endl;
-        std::cout << "Outgoing Edges: " << outgoing_edges << std::endl;
-        // std::cout << "Finished suffix: " << i << std::endl << std::endl;
     }
 
     std::cout << internal_nodes << std::endl;
